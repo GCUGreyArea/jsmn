ec215a2ee53d094594938961d50e56ae1f4f9213
Barry Robinson <barry.w.robinson64@gmail.com>
2024-08-18 08:27:40 +0100

    Add basic test program - needs to be moved into test diorectory

---
+++ test.cpp
@@ -0,0 +1,32 @@
+#include "jsmn.hpp"
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char ** argv) {
+    if(argc == 1) return -1;
+
+    jsmn_parser p(2,argv[1]);
+
+    int count = p.parse();
+    if(count < 0) {
+        return -1;
+    }
+    
+    for(unsigned int i = 0;i < count; i++) {
+        p.print_token(i);
+    }
+
+    p.serialise("test.bin");
+    p.init("{}");
+    p.deserialise("test.bin");
+
+
+    count = p.parsed_tokens();
+    for(unsigned int i = 0;i < count; i++) {
+        p.print_token(i);
+    }
+
+
+    return 0;
+}
