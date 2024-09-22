e2bf42842bb7b02f556cbf37b3fbee683a2d0290
Barry Robinson <barry.w.robinson64@gmail.com>
2024-09-22 16:13:46 +0100

    add unit tests for parser

---
+++ test/testJQParser.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <glog/logging.h>
+
+#include <jsmn.hpp>
+
+TEST(JQParser,testJQParser) {
+    const char* str = ".name.value = \"new\"";
+    int ret = jqpath_parse_string(str);
+    ASSERT_EQ(ret,0);
+
+    str = ".name.value != \"test\"";
+    ret = jqpath_parse_string(str);
+    ASSERT_EQ(ret,0);
+
+    str = ".name[1].value != \"test\"";
+    ret = jqpath_parse_string(str);
+    ASSERT_EQ(ret,0);
+
+    str = ".name[101].value = 12";
+    ret = jqpath_parse_string(str);
+    ASSERT_EQ(ret,0);
+
+    str = ".name[001].value = 100.12";
+    ret = jqpath_parse_string(str);
+    ASSERT_EQ(ret,0);
+
+}
