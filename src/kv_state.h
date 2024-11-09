38d21fcda788e3b49b37d87271c201b6caf28055
Barry Robinson <barry.w.robinson64@gmail.com>
2024-11-09 18:08:21 +0000

    add kv_state.h

---
+++ src/kv_state.h
@@ -0,0 +1,17 @@
+#ifndef KV_STATE
+#define KV_STATE
+
+enum class kv_state {
+    START = 0,
+    KEY   = 1,
+    VALUE = 2,
+    NEXT  = 3
+};
+
+inline kv_state& operator++(kv_state& state, int dummy) {
+	state = static_cast<kv_state>(static_cast<int>(state) + 1);
+
+    return state;
+}
+
+#endif // ! KV_STATE
