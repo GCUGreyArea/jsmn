9e93e3f32f57c25f4c3c6b6a3f46deb87893d6de
Barry Robinson <barry.w.robinson64@gmail.com>
2024-11-22 15:36:14 +0000

    remove call to render

--- test/testJQPathJSMN.cpp
+++ test/testJQPathJSMN.cpp
@@ -18,10 +18,8 @@ TEST(JQPathJSMN, testJSMNJQPathStringValues) {
     ASSERT_TRUE(ret > 0);
     ASSERT_TRUE(path != NULL);
 
-    // p.render();
-
-    // auto * np = p.get_path(path);
-    // ASSERT_TRUE(np != nullptr);
+    auto * np = p.get_path(path);
+    ASSERT_TRUE(np != nullptr);
 
 
 }
@@ -40,10 +38,8 @@ TEST(JQPathJSMN, testJSMNJQPathOpenArrayStringValues) {
     ASSERT_TRUE(ret > 0);
     ASSERT_TRUE(path != NULL);
 
-    // p.render();
-
-    // auto * np = p.get_path(path);
-    // ASSERT_TRUE(np != nullptr);
+    auto * np = p.get_path(path);
+    ASSERT_TRUE(np != nullptr);
 
 
 }
