efb8e9eb7f4425c2b21a069b7d8e21be3e937dd7
Barry Robinson <barry.w.robinson64@gmail.com>
2025-02-15 05:00:49 +0000

    fix tests

--- test/testJQPathJSMN.cpp
+++ test/testJQPathJSMN.cpp
@@ -28,3 +28,48 @@ TEST(JQPathJSMN, testJSMNJQPathStringValues) {
 
     ASSERT_TRUE(*np == "\"value 2\"");
 }
+
+TEST(JQPathJSMN, testJSMNJQPathOpenArrayStringValues) {
+    std::string json = "{\"name\":[\"value 1\",\"value 2\",\"value 3\"]}";
+    std::string path_str = ".name[1] = \"value 2\"";
+
+
+    jsmn_parser p(json,2);
+
+    struct jqpath * path = jqpath_parse_string(path_str.c_str());
+
+    int ret = p.parse();
+
+    ASSERT_TRUE(ret > 0);
+    ASSERT_TRUE(path != NULL);
+
+    p.render();
+
+    auto * np = p.get_path(path);
+    ASSERT_TRUE(np != nullptr);
+
+    ASSERT_TRUE(*np == "\"value 2\"");
+}
+
+TEST(JQPathJSMN, testJSMNJQPathOpenArrayStringValuesComplex) {
+    std::string json = "{\"name\":[\"value 1\",\"value 2\",\"value 3\"]}";
+    std::string path_str = ".name[2] = \"value 3\"";
+
+
+    jsmn_parser p(json,2);
+
+    struct jqpath * path = jqpath_parse_string(path_str.c_str());
+
+    int ret = p.parse();
+
+    ASSERT_TRUE(ret > 0);
+    ASSERT_TRUE(path != NULL);
+
+    p.render();
+
+    auto * np = p.get_path(path);
+    ASSERT_TRUE(np != nullptr);
+
+    ASSERT_TRUE(*np == "\"value 3\"");
+}
+
