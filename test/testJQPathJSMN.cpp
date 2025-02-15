a89770da29e3cf119cf83de6f34458a4ddc68e08
Barry Robinson <barry.w.robinson64@gmail.com>
2025-02-15 14:16:48 +0000

    add unit tests

--- test/testJQPathJSMN.cpp
+++ test/testJQPathJSMN.cpp
@@ -5,8 +5,6 @@
 #include "jqpath.h"
 
 
-
-
 TEST(JQPathJSMN, testJSMNJQPathStringValues) {
     std::string json = "{\"name\":[\"value 1\",\"value 2\",\"value 3\"]}";
     std::string path_str = ".name[1] = \"value 2\"";
@@ -73,3 +71,46 @@ TEST(JQPathJSMN, testJSMNJQPathOpenArrayStringValuesComplex) {
     ASSERT_TRUE(*np == "\"value 3\"");
 }
 
+TEST(JQPathJSMN, testJSMNJQPathOpenArrayStringValuesComplexWithoutEquals) {
+    std::string json = "{\"name\":[\"value 1\",\"value 2\",\"value 3\"]}";
+    std::string path_str = ".name[2]";
+
+
+    jsmn_parser p(json,2);
+
+    struct jqpath * path = jqpath_parse_string(path_str.c_str());
+
+    int ret = p.parse();
+
+    ASSERT_TRUE(ret > 0);
+    ASSERT_TRUE(path != NULL);
+
+    p.render();
+
+    auto * np = p.get_path(path);
+    ASSERT_TRUE(np != nullptr);
+
+    ASSERT_TRUE(*np == "\"value 3\"");
+}
+
+TEST(JQPathJSMN, testJSMNJQPathOpenArrayStringValuesSimpleWithoutEquals) {
+    std::string json = "{\"name\":\"Barry\"}";
+    std::string path_str = ".name";
+
+
+    jsmn_parser p(json,2);
+
+    struct jqpath * path = jqpath_parse_string(path_str.c_str());
+
+    int ret = p.parse();
+
+    ASSERT_TRUE(ret > 0);
+    ASSERT_TRUE(path != NULL);
+
+    p.render();
+
+    auto * np = p.get_path(path);
+    ASSERT_TRUE(np != nullptr);
+
+    ASSERT_TRUE(*np == "\"Barry\"");
+}
